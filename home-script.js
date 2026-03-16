655b717951d9f63f314b6913583e9e9e5187bd56
sadmanhabijsaim <sadmansiam260@gmail.com>
2026-03-17 04:29:31 +0600

    implement dynamic issue card rendering and modal close logic

--- home-script.js
+++ home-script.js
@@ -100,3 +100,51 @@ function showIssueModal(id) {
     `;
     document.getElementById('word_modal').style.display = 'block';
 }
+
+function closeModal() {
+    document.getElementById('word_modal').style.display = 'none';
+}
+
+const displayAll=(data)=>{
+
+    console.log(data);
+    let count=document.getElementById('issue-count');
+    count.innerText=data.length;
+
+    const cardContainer=document.getElementById('card-container');
+    cardContainer.innerHTML="";
+    data.forEach(issue => {
+        const card=document.createElement('div');
+        card.innerHTML=`
+                  <div class="card shadow-sm rounded-md border-t-4 ${issue.status === 'open' ? 'border-t-[#00A96E]' : 'border-t-[#A855F7]'} bg-[#FFFFFF] p-4 min-h-80">
+                <div class=" flex items-center justify-between ">
+                    <img src="${issue.status === 'open' ? './assets/Open-Status.png' : './assets/Closed- Status .png'}" alt="">
+                    <button onclick="showIssueModal(${issue.id})" class="btn btn-soft ${issue.priority === 'high' ? 'btn-error' : issue.priority === 'medium' ? 'btn-warning' : ''} rounded-full px-6">${issue.priority === 'high' ? 'High' : issue.priority === 'medium' ? 'Medium' : 'Low'}</button>
+                </div>
+                <div>
+                    <h2 class="text-lg font-semibold mt-2">${issue.title}</h2>
+                    <p class="text-sm text-[#64748B] mt-1">${issue.description}</p>
+                    <div class="mt-2 flex justify-start gap-2">
+                        ${issue.labels.map((label, index) => {
+                            // Default button classes
+                            let btnClass = 'btn btn-soft btn-outline rounded-full';
+                            // Add color only to first and second buttons
+                            if (index === 0) {
+                                btnClass += ' btn-warning';
+                            } else if (index === 1) {
+                                btnClass += ' btn-error';
+                            }
+                            // Other buttons stay with default outline
+                            return `<button class="${btnClass}">${label}</button>`;
+                        }).join('')}
+                    </div>
+                   <hr class="mt-3 border-t border-[#64748B30]">
+                    <p class="text-sm text-[#64748B] mt-2">#${issue.id} by ${issue.author}</p>
+                    <p class="text-sm text-[#64748B] mt-2">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                </div>
+            </div>
+        `
+        cardContainer.appendChild(card);
+    });
+    manageSpinner(false);
+}
